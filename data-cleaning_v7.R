aef28e659256a7b4c94816343f1c559cb70a71e5
EconMaett <matthias.spichiger@bluewin.ch>
2023-09-22 14:27:08 +0200

    Initial commit

--- data-cleaning_v7.R
+++ data-cleaning_v7.R
@@ -1,6 +1,7 @@
 # data-cleaning_v7.R
 
-devtools::install(pkg = "C:/Users/matth/OneDrive/Dokumente/R-pkgs/golf")
+# devtools::install(pkg = "C:/Users/matth/OneDrive/Dokumente/R-pkgs/golf")
+devtools::install_github(repo = "https://github.com/EconMaett/golf.git")
 library(golf)
 library(tidyverse)
 
@@ -21,4 +22,8 @@ infile <- "swim.csv"
 write_csv(dat, outfile_path(infile))
 
 citation("golf")
+
+golf::mfl2()
+golf::set_mfl2(l = c("j", "k", "l"))
+golf::mfl2()
 # END
